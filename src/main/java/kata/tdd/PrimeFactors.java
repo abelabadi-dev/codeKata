77b927b5866c07ccb9b82ad97f18406f27fdc90b
abel abadi <abelabadi2@gmail.com>
2016-08-06 15:12:49 -0500

    of_1_isEmpty, of_2_is2 , of_3_is3
    passed test

--- src/main/java/kata/tdd/PrimeFactors.java
+++ src/main/java/kata/tdd/PrimeFactors.java
@@ -1,12 +1,21 @@
 package kata.tdd;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PrimeFactors {
 
-	public static ArrayList<Integer> of(int i) {
+	public static List<Integer> of(int i) {
 		
-		return new ArrayList<Integer>();
+		List<Integer> factors = new ArrayList<>();
+		
+		if(i == 1)
+			return factors;
+		
+		factors.add(i);
+		return factors;
 	}
-
+	
+	
+	
 }
--- src/test/java/kata/tdd/PrimeFactorsTest.java
+++ src/test/java/kata/tdd/PrimeFactorsTest.java
@@ -3,6 +3,7 @@ package kata.tdd;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -15,8 +16,11 @@ public class PrimeFactorsTest {
 	
 	@Test
 	public void of_2_is_2()  {
-		
+		assertEquals(PrimeFactors.of(2),new ArrayList<Integer>(Arrays.asList(2)));
 	}
 	
-	
+	@Test
+	public void of_3_is_3() {
+		assertEquals(PrimeFactors.of(3),new ArrayList<Integer>(Arrays.asList(3)));
+	}
 }
